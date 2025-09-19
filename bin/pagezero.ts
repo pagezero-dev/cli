de07fba552a24f3b935b6bfb1adbe85eef199a78
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-19 13:52:10 +0200

    Introduce cli args handling

--- bin/pagezero.ts
+++ bin/pagezero.ts
@@ -1,3 +1,28 @@
 #!/usr/bin/env bun
 
-console.log("pagezero - CLI tool")
+import { input } from "@inquirer/prompts"
+import chalk from "chalk"
+import { program } from "commander"
+import ora from "ora"
+
+program
+  .description("PageZERO CLI")
+  .option("-h, --help", "output usage information")
+
+program
+  .command("init")
+  .description("initialize a new project")
+  .action(async () => {
+    console.log(chalk.green("👋 Welcome to PageZERO CLI"))
+    console.log(chalk.green("🚀 Let's get you started with your project!"))
+    const projectName = await input({
+      message: "What is the name of your project?",
+    })
+    const spinner = ora(`Creating project ${projectName}...`).start()
+    setTimeout(() => {
+      spinner.suffixText = " done"
+      spinner.succeed()
+    }, 2000)
+  })
+
+program.parse()
