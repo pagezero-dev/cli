17381f7e9048b31af5de6a88bc063b69cd22bb53
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-19 14:20:11 +0200

    Init command implementation (#2)

--- bin/pagezero.ts
+++ bin/pagezero.ts
@@ -1,3 +1,46 @@
 #!/usr/bin/env bun
 
-console.log("pagezero - CLI tool")
+import { input } from "@inquirer/prompts"
+import { $ } from "bun"
+import chalk from "chalk"
+import { program } from "commander"
+import ora from "ora"
+
+program
+  .description("PageZERO CLI")
+  .option("-h, --help", "output usage information")
+
+program.command("init").description("initialize a new project").action(init)
+
+program.parse()
+
+async function init() {
+  // Welcome
+  console.log(chalk.green("👋 Welcome to PageZERO CLI"))
+  console.log(chalk.green("🚀 Let's get you started with your project!"))
+
+  // Bootstrap the project
+  const projectName = await input({
+    message: "What is the name of your project?",
+  })
+  const creatingProjectSpinner = ora(
+    `Running: bun create pagezero-dev/pagezero --no-install ${projectName}`,
+  ).start()
+  await $`bun create pagezero-dev/pagezero --no-install ${projectName}`.quiet()
+  creatingProjectSpinner.succeed()
+
+  // Install dependencies
+  const installingDependenciesSpinner = ora(`Running: bun install`).start()
+  await $`cd ${projectName} && bun install`.quiet()
+  installingDependenciesSpinner.succeed()
+
+  // Run setup script
+  const runningSetupScriptSpinner = ora(`Running: bun run setup`).start()
+  await $`cd ${projectName} && bun run setup`.quiet()
+  runningSetupScriptSpinner.succeed()
+
+  // Done
+  console.log(chalk.green("🎉 Done! Your project is ready to go."))
+  console.log(chalk.green.bold(`cd ${projectName}`))
+  console.log(chalk.green.bold("bun run dev"))
+}
