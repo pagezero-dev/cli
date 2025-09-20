d91494425ccba472cd7b2366d508e44286bdd57f
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-20 10:24:37 +0200

    feat: updating wrangler.json config with project name

--- bin/pagezero.ts
+++ bin/pagezero.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
 import { input } from "@inquirer/prompts"
-import { $ } from "bun"
+import { $, file, write } from "bun"
 import chalk from "chalk"
 import { program } from "commander"
 import ora from "ora"
@@ -31,13 +31,37 @@ async function init() {
 
   // Install dependencies
   const installingDependenciesSpinner = ora(`Running: bun install`).start()
-  await $`cd ${projectName} && bun install`.quiet()
+  await $`bun install`.quiet().cwd(projectName)
   installingDependenciesSpinner.succeed()
 
   // Run setup script
   const runningSetupScriptSpinner = ora(`Running: bun run setup`).start()
-  await $`cd ${projectName} && bun run setup`.quiet()
+  await $`bun run setup`.quiet().cwd(projectName)
   runningSetupScriptSpinner.succeed()
+
+  // Update wrangler.json
+  const updatingWranglerJsonSpinner = ora(`Updating wrangler.json`).start()
+  try {
+    const wranglerJson = await file(`${projectName}/wrangler.json`).json()
+    wranglerJson.name = projectName
+    wranglerJson.d1_databases[0].database_name = `${projectName}-development`
+    wranglerJson.env.production.d1_databases[0].database_name = `${projectName}-production`
+    wranglerJson.env.preview.d1_databases[0].database_name = `${projectName}-preview`
+    wranglerJson.env.test.d1_databases[0].database_name = `${projectName}-test`
+    wranglerJson.env.production.d1_databases[0].database_id = "<DATABASE_ID>"
+    wranglerJson.env.preview.d1_databases[0].database_id = "<DATABASE_ID>"
+    await write(
+      `${projectName}/wrangler.json`,
+      JSON.stringify(wranglerJson, null, 2),
+    )
+    updatingWranglerJsonSpinner.succeed()
+  } catch (error) {
+    updatingWranglerJsonSpinner.warn()
+    console.error(chalk.yellow("Issue with updating wrangler.json"))
+    if (error instanceof Error) {
+      console.error(chalk.yellow(error.message))
+    }
+  }
 
   // Done
   console.log(chalk.green("🎉 Done! Your project is ready to go."))
