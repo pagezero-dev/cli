df269633e71f4efd59b2b38ce9a9821837e9f388
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-09-20 12:06:05 +0200

    feat: upgrade command

--- bin/pagezero.ts
+++ bin/pagezero.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env bun
 
-import { input } from "@inquirer/prompts"
+import { confirm, input } from "@inquirer/prompts"
+import boxen from "boxen"
 import { $, file, write } from "bun"
 import chalk from "chalk"
 import { program } from "commander"
+import logSymbols from "log-symbols"
 import ora from "ora"
 
 program
@@ -11,6 +13,8 @@ program
   .option("-h, --help", "output usage information")
 
 program.command("init").description("initialize a new project").action(init)
+
+program.command("upgrade").description("upgrade pagezero stack").action(upgrade)
 
 program.parse()
 
@@ -67,4 +71,60 @@ async function init() {
   console.log(chalk.green("🎉 Done! Your project is ready to go."))
   console.log(chalk.green.bold(`cd ${projectName}`))
   console.log(chalk.green.bold("bun run dev"))
+}
+
+async function upgrade() {
+  console.log(
+    chalk.yellow(
+      boxen(
+        "Ugrade command is primitive. It will overwrite your existing \nproject files with the latest version of the PageZERO stack. \nAfterwards please review the changes through a git diff.",
+        { padding: 1, title: "WARNING", titleAlignment: "center" },
+      ),
+    ),
+  )
+
+  const shouldProceed = await confirm({
+    message: "Do you want to proceed?",
+  })
+
+  if (!shouldProceed) {
+    return
+  }
+
+  const rsync = await $`command -v rsync`.quiet()
+  if (rsync.stdout) {
+    console.log(`${logSymbols.success} rsync is installed`)
+  } else {
+    console.log(
+      `${logSymbols.error} rsync is not installed and is required for the upgrade command: please install it and try again`,
+    )
+    return
+  }
+
+  const wranglerFile = file(`wrangler.json`)
+  if (await wranglerFile.exists()) {
+    console.log(`${logSymbols.success} wrangler.json file found`)
+  } else {
+    console.log(
+      `${logSymbols.error} wrangler.json file not found: please run command within PageZERO project directory`,
+    )
+    return
+  }
+
+  const clonePagezeroSpinner = ora(`downloading latest PageZERO stack`).start()
+  await $`git clone --depth 1 https://github.com/pagezero-dev/pagezero.git pagezero-latest`.quiet()
+  clonePagezeroSpinner.succeed()
+
+  const copyPagezeroSpinner = ora(
+    `copying PageZERO stack to project directory`,
+  ).start()
+  await $`rsync -a --exclude=".git" ./pagezero-latest/ ./`.quiet()
+  copyPagezeroSpinner.succeed()
+
+  const cleanupSpinner = ora(`cleaning up`).start()
+  await $`rm -rf pagezero-latest`.quiet()
+  cleanupSpinner.succeed()
+
+  console.log(chalk.green("PageZERO stack upgraded successfully"))
+  console.log(chalk.green.bold("Please review the changes through a git diff"))
 }
