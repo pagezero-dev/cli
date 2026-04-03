4921f50a09bb26923c4e6e72c892b975434057b0
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2026-04-03 15:25:23 +1100

    refactor(cli): remove powerup edition option (#12)

--- bin/pagezero.ts
+++ bin/pagezero.ts
@@ -8,16 +8,8 @@ program
   .description("PageZERO CLI")
   .option("-h, --help", "output usage information")
 
-program
-  .command("init")
-  .description("initialize a new project")
-  .option("-p, --powerup", "use PowerUP edition")
-  .action(init)
+program.command("init").description("initialize a new project").action(init)
 
-program
-  .command("upgrade")
-  .description("upgrade pagezero stack")
-  .option("-p, --powerup", "use PowerUP edition")
-  .action(upgrade)
+program.command("upgrade").description("upgrade pagezero stack").action(upgrade)
 
 program.parse()
